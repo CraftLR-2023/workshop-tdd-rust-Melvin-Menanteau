8cf686ca65c4c10ae5f12eb10bb1b250b71df922
Melvin MENANTEAU <melvin.menanteau@soriba.fr>
2023-09-18 16:22:54 +0200

    feat (exercice3): exercice3

--- exercice3/src/lib.rs
+++ exercice3/src/lib.rs
@@ -1,5 +1,33 @@
+fn convert(c: char) -> u32 {
+	match c {
+		'I' => 1,
+		'V' => 5,
+		'X' => 10,
+		'L' => 50,
+		'C' => 100,
+		'D' => 500,
+		'M' => 1000,
+		_ => 0
+	}
+}
+
 pub fn en_nombre_arabe(_nombre_romain: &str) -> u32 {
-  1
+	let mut r = 0;
+	
+	for (index, number) in _nombre_romain.char_indices() {
+		if index > 0 {
+			r += convert(number);
+
+			if convert(_nombre_romain.chars().nth(index - 1).unwrap()) < convert(number) {
+				r -= convert(_nombre_romain.chars().nth(index - 1).unwrap()) * 2;
+			}
+		} else {
+			r += convert(number);
+		}
+		println!("{}", r);
+	}
+
+	r
 }
 
 #[cfg(test)]
@@ -12,112 +40,96 @@ mod tests {
     assert_eq!(1, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_ii_devrait_donner_2() {
     let nombre = en_nombre_arabe("II");
     assert_eq!(2, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_iii_devrait_donner_3() {
     let nombre = en_nombre_arabe("III");
     assert_eq!(3, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_v_devrait_donner_5() {
     let nombre = en_nombre_arabe("V");
     assert_eq!(5, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_vi_devrait_donner_6() {
     let nombre = en_nombre_arabe("VI");
     assert_eq!(6, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_iv_devrait_donner_4() {
     let nombre = en_nombre_arabe("IV");
     assert_eq!(4, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_x_devrait_donner_10() {
     let nombre = en_nombre_arabe("X");
     assert_eq!(10, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_xi_devrait_donner_11() {
     let nombre = en_nombre_arabe("XI");
     assert_eq!(11, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_l_devrait_donner_50() {
     let nombre = en_nombre_arabe("L");
     assert_eq!(50, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_xlix_devrait_donner_49() {
     let nombre = en_nombre_arabe("XLIX");
     assert_eq!(49, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_c_devrait_donner_100() {
     let nombre = en_nombre_arabe("C");
     assert_eq!(100, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_xcix_devrait_donner_99() {
     let nombre = en_nombre_arabe("XCIX");
     assert_eq!(99, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_cccxlvi_devrait_donner_346() {
     let nombre = en_nombre_arabe("CCCXLVI");
     assert_eq!(346, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_cccxcix_devrait_donner_399() {
     let nombre = en_nombre_arabe("CCCXCIX");
     assert_eq!(399, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_d_devrait_donner_500() {
     let nombre = en_nombre_arabe("D");
     assert_eq!(500, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_m_devrait_donner_1000() {
     let nombre = en_nombre_arabe("M");
     assert_eq!(1000, nombre);
   }
 
-  #[ignore]
   #[test]
   fn le_chiffre_romain_mmmmdccclxxxviii_devrait_donner_4888() {
     let nombre = en_nombre_arabe("MMMMDCCCLXXXVIII");
