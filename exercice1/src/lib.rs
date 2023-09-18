de675cae3eddcfcc1987e2f4e3ca580ac07b8f30
Melvin MENANTEAU <melvin.menanteau@soriba.fr>
2023-09-18 14:09:32 +0200

    feat (exercice1): exercice1

--- exercice1/src/lib.rs
+++ exercice1/src/lib.rs
@@ -1,25 +1,28 @@
 // &'static est un "lifetime specifier", vous verrez plus tard à quoi cela correspond
 pub fn hello(_s1: &str) -> String {
-  String::new()
+    if !_s1.is_empty() {
+        format!("Hello, {}!", _s1)
+    } else {
+        String::from("Hello, World!")
+    }
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
-  #[ignore]
+
   #[test]
   fn test_hello_with_empty_string_is_compared_by_value() {
     assert_eq!("Hello, World!", hello(""));
   }
 
-  #[ignore]
+  
   #[test]
   fn test_hello_should_return_hello_alice_when_alice() {
     assert_eq!("Hello, Alice!", hello("Alice"));
   }
 
-  #[ignore]
   #[test]
   fn test_hello_should_return_hello_bob_when_bob() {
     assert_eq!("Hello, Bob!", hello("Bob"));
